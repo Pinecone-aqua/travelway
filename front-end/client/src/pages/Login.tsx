32a9fc7df32a196cb3dc4739b737501c475441bb
Yalalt <yalaltgandush@gmail.com>
2023-04-11 11:18:39 +0800

    start login

--- front-end/client/src/pages/Login.tsx
+++ front-end/client/src/pages/Login.tsx
@@ -1,5 +1,15 @@
+import Head from "next/head";
 import React from "react";
 
 export default function Login() {
-  return <div className="bg-slate-100 h-85vh">Login</div>;
+  return <div className="bg-slate-100 h-85vh">
+    <Head>
+        <title>Хэрэглэгч нэвтрэх</title>
+    </Head>
+    Login</div>;
+}
+
+export default getStaticProps() {
+
+    return {}
 }
--- front-end/client/src/pages/index.tsx
+++ front-end/client/src/pages/index.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>HOME a PAGE</div>
+    <div>
+      
+      HOME a PAGE
+      <br/>
+      <Link href="/login">Login</Link>
+    </div>
   )
 }
