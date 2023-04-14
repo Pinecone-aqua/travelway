81f1afca51f0850fac8ad68e4c785d9917a7f852
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-04-14 10:02:08 +0800

    dashboard crud

--- front-end/dashboard/src/pages/users.tsx
+++ front-end/dashboard/src/pages/users.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface userType {
+  id: "",
+  firstname: "",
+  lastname: "",
+  email: "",
+  phone: 0,
+  password: ""
+}
+
 export default function Users(): JSX.Element {
+  const[user,setUser]= useState();
+
+  useEffect(()=>{
+    axios.get("http://localhost:9090/users/get").then((user)=>setUser(user.data))
+  },[]);
+
+  console.log("user",user);
+  
   return (
+  
     <>
-      <div className="bg-violet-400 rounded-2xl w-10/12 h-2/3 "> users</div>
+      <div className="bg-violet-400 rounded-2xl w-10/12 h-2/3 "> 
+      <div>{user?.map((unit: userType, index: string) => (
+
+
+        ))}</div>
+      
+      </div>
     </>
   );
 }
