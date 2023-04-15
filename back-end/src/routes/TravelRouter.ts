3222ccfeb11dc13492e904572012cab7d0c004e6
Yalalt <yalaltgandush@gmail.com>
2023-04-15 13:16:02 +0800

    I was created all ids return function in BE router and controller folders

--- back-end/src/routes/TravelRouter.ts
+++ back-end/src/routes/TravelRouter.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/create', travelController.createTravel);
 router.get('/get/:travelId', travelController.readTravel);
 router.get('/get/', travelController.readAllTravel);
+router.get('/getid/', travelController.readTravelAllIDs);
 router.patch('/update/:travelId', travelController.updateTravel);
 router.delete('/delete/:travelId', travelController.deleteTravel);
 
