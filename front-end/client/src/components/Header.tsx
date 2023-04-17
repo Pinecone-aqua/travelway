2e605b4c12056d4dadbe882376b04ff024f888f0
Namnantseren <namka1472@gmail.com>
2023-04-17 23:27:44 +0800

    navigation

--- front-end/client/src/components/Header.tsx
+++ front-end/client/src/components/Header.tsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
+
 export default function Header(): JSX.Element {
+  const [nav, useNav] = useState()
+  function changeNav(e: any) {
+    useNav(e.target.innerText)
+  }
   return (
     <div className="relative">
       <div className="flex gap-3 justify-center content-center text-center  text-white  ">
-        <div className="flex w-[15rem] h-7 mt-3">
-          <button className="w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50">
+        <div className="flex w-[15rem] h-7">
+          <button className={nav == "Home" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
             Home
           </button>
-          <button className="w-[15rem] md:w-[10rem] sm:w-[7rem] opacity-70">
+          <button className={nav == "Our company" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
             Our company
           </button>
         </div>
-        <button className="text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1">
+        <button className={nav == "Travel" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
           Travel
         </button>
-        <div className="flex w-[15rem] h-7 mt-3">
-          <button className="w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-70">
+        <div className="flex w-[15rem] h-7 ">
+          <button className={nav == "Gudget" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
             Gudget
           </button>
-          <button className="w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50">
+          <button className={nav == "Contact us" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
             Contact us
           </button>
         </div>
