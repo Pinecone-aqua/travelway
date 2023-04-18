0c24790b4b9668fcb3585c2ce02e9a27d77647d2
Morning <morning@admins-iMac-1-6.local>
2023-04-18 13:05:13 +0800

    change eslint

--- front-end/client/src/components/Header.tsx
+++ front-end/client/src/components/Header.tsx
@@ -1,29 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 
 export default function Header(): JSX.Element {
-  const [nav, useNav] = useState()
+  const [nav, setNav] = useState<null | string>();
+
   function changeNav(e: any) {
-    useNav(e.target.innerText)
+    setNav(e.target.innerText);
   }
   return (
     <div className="relative">
       <div className="flex gap-3 justify-center content-center text-center  text-white  ">
         <div className="flex w-[15rem] h-7">
-          <button className={nav == "Home" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
+          <button
+            className={
+              nav == "Home"
+                ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1"
+                : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"
+            }
+            onClick={changeNav}
+          >
             Home
           </button>
-          <button className={nav == "Our company" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
+          <button
+            className={
+              nav == "Our company"
+                ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1"
+                : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"
+            }
+            onClick={changeNav}
+          >
             Our company
           </button>
         </div>
-        <button className={nav == "Travel" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
+        <button
+          className={
+            nav == "Travel"
+              ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1"
+              : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"
+          }
+          onClick={changeNav}
+        >
           Travel
         </button>
         <div className="flex w-[15rem] h-7 ">
-          <button className={nav == "Gudget" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
+          <button
+            className={
+              nav == "Gudget"
+                ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1"
+                : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"
+            }
+            onClick={changeNav}
+          >
             Gudget
           </button>
-          <button className={nav == "Contact us" ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1" : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"} onClick={changeNav}>
+          <button
+            className={
+              nav == "Contact us"
+                ? "text-[30px] w-[15rem] md:w-[10rem] sm:w-[8rem] mt-1"
+                : "w-[15rem] md:w-[10rem] sm:w-[5rem] opacity-50 mt-3"
+            }
+            onClick={changeNav}
+          >
             Contact us
           </button>
         </div>
