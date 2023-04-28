49d3b290228ea14ca63b37a4430987646baf378a
Yalalt <yalaltgandush@gmail.com>
2023-04-28 10:57:23 +0800

    Context user ..

--- front-end/client/src/context/user.context.ts
+++ front-end/client/src/context/user.context.ts
@@ -1,5 +1,25 @@
-import React from "react";
+import { createContext, ReactNode, useContext, useEffect } from "react";
 
-const UserContext = React.createContext(null);
+interface UserContextType {
+  currentUser: string;
+}
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export function useUser() {
+  return useContext(UserContext);
+}
+
+interface UserProviderType {
+  children: ReactNode;
+}
+
+export default function UserProvider({ children }: UserProviderType) {
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+  });
+}
 
 export default UserContext;
