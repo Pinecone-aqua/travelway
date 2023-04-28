a0beed1500d7c75a52a7e142638ea774534dc302
Morning <morning@admins-iMac-1-6.local>
2023-04-28 09:37:24 +0800

    added some user data

--- front-end/client/src/components/userProfile/TravelGuide.tsx
+++ front-end/client/src/components/userProfile/TravelGuide.tsx
@@ -1,3 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 export default function TravelGuide() {
-  return <>TravelGuide</>;
+  return (
+    <>
+      <div className=" flex justify-center">
+        <img src="./images/work.webp" alt="pic" />
+      </div>
+    </>
+  );
 }
