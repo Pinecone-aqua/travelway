71f312269f02e04ceabb787bb822623e5b68978c
Yalalt <yalaltgandush@gmail.com>
2023-05-04 08:07:28 +0800

    travelplan edited

--- front-end/client/src/pages/tour/travelplan.tsx
+++ front-end/client/src/pages/tour/travelplan.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { FormEvent, useEffect, useState } from "react";
-import { TravelType } from "../../../util/types";
+import { DayType, TravelType } from "../../../util/types";
 import Image from "next/image";
 
 export default function TravelPlan(): JSX.Element {
@@ -24,6 +24,9 @@ export default function TravelPlan(): JSX.Element {
     setCurrentPage(index);
   };
 
+  console.log("Data-page ===>" , pageNumber);
+  console.log("Data===> ", travels);
+
   return (
     <div>
       <div>
@@ -43,7 +46,7 @@ export default function TravelPlan(): JSX.Element {
             ))}
           </div>
           <div className="w-full bg-slate-50 p-2 text-start">
-            {travels.map((page, index) =>
+            {/* {travels.map(({ title, description, day }: TravelType, index) =>
               pageNumber === index ? (
                 <div key={index}>
                   <div
@@ -51,127 +54,118 @@ export default function TravelPlan(): JSX.Element {
                     key={index}
                   >
                     <h1 className="text-3xl text-start p-2 mb-6 text-orange-700">
-                      {page.title}
+                      {travel.title}
                     </h1>
                     <p className="w-full text-justify text-lg mt-4 mb-10">
-                      {page.description}
+                      {travel.description}
                     </p>
                     <div>
-                      {page.day.map((oneday, lindex) => (
-                        <div key={lindex}>
-                          {lindex === 0 ? (
-                            <div className="my-4 w-full overflow-hidden">
-                              <Image
-                                src={oneday.image}
-                                alt={oneday.image}
-                                width={1450}
-                                height={300}
-                                className="rounded-md w-full h-96 object-cover"
-                              />
-
-                              <h2 className="font-bold text-slate-700 text-4xl mt-4 mb-2">
-                                Day {lindex + 1}
-                              </h2>
-                              {/* <p className="text-xl text-orange-500 font-normal">
-                                {Date(page.updatedAt)}
-                              </p> */}
-                              <h3 className="font-bold mb-2 text-lg">
-                                {oneday.subTitle}
-                              </h3>
-                              <p className="my-4 text-xl text-justify">
-                                {oneday.describe}
-                              </p>
-                              <h3 className="font-bold mt-2">
-                                Очих газар: {oneday.destination}
-                              </h3>
-                              <h3 className="text-normal text-lg">
-                                <span className="font-bold">
-                                  Анхаарах зүйл:{" "}
-                                </span>
-                                {oneday.considerations}
-                              </h3>
-                            </div>
-                          ) : lindex % 2 === 0 ? (
-                            <div className="flex justify-between gap-20 items-center mt-8 mb-4">
-                              <div className="flex-0">
-                                <Image
-                                  src={oneday.image}
-                                  alt={oneday.image}
-                                  width={550}
-                                  height={400}
-                                  className="rounded-md"
-                                />
-                              </div>
-                              <div className="flex-1">
-                                <h2 className="font-bold text-4xl mt-4 mb-2">
-                                  Day {lindex + 1}
-                                </h2>
-                                {/* <p className="text-xl text-orange-500 font-normal">
-                                  {Date(page.updatedAt)}
-                                </p> */}
-                                <h3 className="font-bold mb-2 text-lg">
-                                  {oneday.subTitle}
-                                </h3>
-                                <p className="my-4 text-xl text-justify">
-                                  {oneday.describe}
-                                </p>
-                                <h3 className="font-bold mt-2">
-                                  Очих газар: {oneday.destination}
-                                </h3>
-                                <h3 className="text-normal text-lg">
-                                  <span className="font-bold">
-                                    Анхаарах зүйл:{" "}
-                                  </span>
-                                  {oneday.considerations}
-                                </h3>
-                              </div>
-                            </div>
-                          ) : (
-                            <div className="flex justify-between gap-20 items-center mt-8 mb-4">
-                              <div className="flex-1">
-                                <h2 className="font-bold text-4xl mt-4 mb-2">
-                                  Day {lindex + 1}
-                                </h2>
-                                {/* <p className="text-xl text-orange-500 font-normal">
-                                  {Date(page.updatedAt)}
-                                </p> */}
-                                <h3 className="font-bold mb-2 text-lg">
-                                  {oneday.subTitle}
-                                </h3>
-                                <p className="my-4 text-xl text-justify">
-                                  {oneday.describe}
-                                </p>
-                                <h3 className="font-bold mt-2">
-                                  Очих газар: {oneday.destination}
-                                </h3>
-                                <h3 className="text-normal text-lg">
-                                  <span className="font-bold">
-                                    Анхаарах зүйл:{" "}
-                                  </span>
-                                  {oneday.considerations}
-                                </h3>
-                              </div>
-                              <div className="flex-0">
-                                <Image
-                                  src={oneday.image}
-                                  alt={oneday.image}
-                                  width={550}
-                                  height={400}
-                                  className="rounded-md"
-                                />
-                              </div>
-                            </div>
-                          )}
-                        </div>
-                      ))}
+                      <Travel daysOfTravel={travel.day} />
                     </div>
                   </div>
                 </div>
               ) : null
-            )}
+            )} */}
           </div>
         </section>
       </div>
     </div>
   );
 }
+
+// function Travel(daysOfTravel: DayType[]) {
+
+//   return (
+//     <div>
+//       {daysOfTravel.map((dayOf: DayType, index: number) => (
+//         <div key={index}>
+//           {index === 0 ? (
+//             <div className="my-4 w-full overflow-hidden">
+//               <Image
+//                 src={dayOf.image}
+//                 alt={dayOf.image}
+//                 width={1450}
+//                 height={300}
+//                 className="rounded-md w-full h-96 object-cover"
+//               />
+
+//               <h2 className="font-bold text-slate-700 text-4xl mt-4 mb-2">
+//                 Day {index + 1}
+//               </h2>
+//               {/* <p className="text-xl text-orange-500 font-normal">
+//                               {Date(page.updatedAt)}
+//                             </p> */}
+//               <h3 className="font-bold mb-2 text-lg">{dayOf.subTitle}</h3>
+//               <p className="my-4 text-xl text-justify">{dayOf.describe}</p>
+//               <h3 className="font-bold mt-2">
+//                 Очих газар: {dayOf.destination}
+//               </h3>
+//               <h3 className="text-normal text-lg">
+//                 <span className="font-bold">Анхаарах зүйл: </span>
+//                 {dayOf.considerations}
+//               </h3>
+//             </div>
+//           ) : index % 2 === 0 ? (
+//             <div className="flex justify-between gap-20 items-center mt-8 mb-4">
+//               <div className="flex-0">
+//                 <Image
+//                   src={dayOf.image}
+//                   alt={dayOf.image}
+//                   width={550}
+//                   height={400}
+//                   className="rounded-md"
+//                 />
+//               </div>
+//               <div className="flex-1">
+//                 <h2 className="font-bold text-4xl mt-4 mb-2">
+//                   Day {index + 1}
+//                 </h2>
+//                 {/* <p className="text-xl text-orange-500 font-normal">
+//                                 {Date(page.updatedAt)}
+//                               </p> */}
+//                 <h3 className="font-bold mb-2 text-lg">{dayOf.subTitle}</h3>
+//                 <p className="my-4 text-xl text-justify">{dayOf.describe}</p>
+//                 <h3 className="font-bold mt-2">
+//                   Очих газар: {dayOf.destination}
+//                 </h3>
+//                 <h3 className="text-normal text-lg">
+//                   <span className="font-bold">Анхаарах зүйл: </span>
+//                   {dayOf.considerations}
+//                 </h3>
+//               </div>
+//             </div>
+//           ) : (
+//             <div className="flex justify-between gap-20 items-center mt-8 mb-4">
+//               <div className="flex-1">
+//                 <h2 className="font-bold text-4xl mt-4 mb-2">
+//                   Day {index + 1}
+//                 </h2>
+//                 {/* <p className="text-xl text-orange-500 font-normal">
+//                                 {Date(page.updatedAt)}
+//                               </p> */}
+//                 <h3 className="font-bold mb-2 text-lg">{dayOf.subTitle}</h3>
+//                 <p className="my-4 text-xl text-justify">{dayOf.describe}</p>
+//                 <h3 className="font-bold mt-2">
+//                   Очих газар: {dayOf.destination}
+//                 </h3>
+//                 <h3 className="text-normal text-lg">
+//                   <span className="font-bold">Анхаарах зүйл: </span>
+//                   {dayOf.considerations}
+//                 </h3>
+//               </div>
+//               <div className="flex-0">
+//                 <Image
+//                   src={dayOf.image}
+//                   alt={dayOf.image}
+//                   width={550}
+//                   height={400}
+//                   className="rounded-md"
+//                 />
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
