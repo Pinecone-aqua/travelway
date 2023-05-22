85fe5ca624932d8d62c7fdc583b24591f3bc70f2
Yalalt <yalaltgandush@gmail.com>
2023-05-22 09:02:22 +0800

    env file edit

--- be-nestjs/src/cloudinary/cloudinary.provider.ts
+++ be-nestjs/src/cloudinary/cloudinary.provider.ts
@@ -4,9 +4,9 @@ export const CloudinaryProvider = {
   provide: `${process.env.CLOUDINARY}`,
   useFactory: () => {
     return v2.config({
-      cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
-      api_key: `${process.env.CLOUDINARY_API_KEY}`,
-      api_secret: `${process.env.CLOUDINARY_API_SECRET}`,
+      cloud_name: `${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}`,
+      api_key: `${process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY}`,
+      api_secret: `${process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET}`,
     });
   },
 };
